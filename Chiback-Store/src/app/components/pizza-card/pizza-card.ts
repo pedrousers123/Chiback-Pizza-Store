f0a999f277b7cabc8cac0f738404daea9906f9ea
Pedro <pedroluacas1234@gmail.com>
2026-08-06 19:41:34 -0300

    Adiciona components

---
+++ Chiback-Store/src/app/components/pizza-card/pizza-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-card',
+  imports: [],
+  templateUrl: './pizza-card.html',
+  styleUrl: './pizza-card.css',
+})
+export class PizzaCard {}
---
+++ Chiback-Store/src/app/components/pizza-list/pizza-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-list',
+  imports: [],
+  templateUrl: './pizza-list.html',
+  styleUrl: './pizza-list.css',
+})
+export class PizzaList {}
